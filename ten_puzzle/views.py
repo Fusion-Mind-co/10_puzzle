cb2f6752fd60a28fb34fd5c4ab898548248b652a
Fusion-Mind-co <fusionmind.co@gmail.com>
2025-10-03 18:03:15 +0900

    UIの調整

--- ten_puzzle/views.py
+++ ten_puzzle/views.py
@@ -9,8 +9,11 @@ def game_page(request):
     random_id = random.randint(1, 552)
 
     data = Problem.objects.get(id=random_id)
+    # クリア数+1 = 何問目
+    cleared_count = Problem.objects.filter(is_cleared=True).count() + 1
     context = {
-        'data': data 
+        'data': data ,
+        'cleared_count' : cleared_count
     }
     print(f'取得データ = {data}')
     return render(request, 'game.html', context)
